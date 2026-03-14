ff083c056add5e44e71fd487e21a75cd8c345b1d
Szymon <rospondek.szymon@gmail.com>
2026-03-14 12:19:00 +0100

    Dodano komentarz przy teście tymczasowym

--- tests/functional/test_functional.py
+++ tests/functional/test_functional.py
@@ -1,5 +1,6 @@
 import imageio.v3 as iio
 
+# Do usunięcia w dalszych iteracjach projektu
 def test_placeholder():
     read_img = iio.imread('imageio:chelsea.png')
     assert read_img.shape is not None
